8423719c43a94aef0f807c0c361f73234cc43d39
dalton-herriman <dalton.herriman76@icloud.com>
2025-09-01 14:47:24 -0500

    added an example of linear search, faster than hash table lookup @ ~7 items or less

--- hashmap.c
+++ hashmap.c
@@ -5,3 +5,12 @@ typedef struct {
     int value;
 } item;
 
+// linear search, faster than hashtable lookup up to ~7 items
+item* linear_search(item* items, size_t size, const char* key) {
+    for (size_t i=0; i<size; i++) {
+        if (strcmp(items[i].key, key) == 0) {
+            return &items[i];
+        }
+    }
+    return NULL;
+}
